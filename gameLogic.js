e6e6f53941ce02911c2e192bc4ee8787ce3db1a1
Sebastian <svamediamarketing@gmail.com>
2021-11-22 14:07:32 -0500

    win() now displays the gameMessage on the webpage

--- gameLogic.js
+++ gameLogic.js
@@ -3,7 +3,7 @@ let computerScore = 0;
 const playerScore_span = document.getElementById("userScore");
 const computerScore_span = document.getElementById("computerScore");
 const scoreBoard_div = document.querySelector(".scorecard");
-const gameMessage_div = document.querySelector(".game-message");
+const gameMessage_p = document.querySelector(".game-message > p");
 const paper_input = document.getElementById("p");
 const rock_input = document.getElementById("r");
 const scissor_input = document.getElementById("s");
@@ -14,10 +14,12 @@ function getComputerSelection() {
   return choices[randomNumber];
 }
 
-function win() {
+function win(playerSelection, computerSelection) {
   playerScore++;
   playerScore_span.innerHTML = playerScore;
   computerScore_span.innerHTML = computerScore;
+  gameMessage_p.innerHTML =
+    playerSelection + " beats " + computerSelection + ". You WIN!";
 }
 
 function lose() {
@@ -34,17 +36,17 @@ function game(playerSelection) {
     case "rockscissors":
     case "paperrock":
     case "scissorspaper":
-      win();
+      win(playerSelection, computerSelection);
       break;
     case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
-      lose();
+      lose(playerSelection, computerSelection);
       break;
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
-      tie();
+      tie(playerSelection, computerSelection);
       break;
   }
 }
